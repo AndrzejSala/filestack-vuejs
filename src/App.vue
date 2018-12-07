0c2675e52b28f5d92069b84a354afbd83a1a90ec
asala <Andrzej.Sala@dreamlab.pl>
2018-12-07 12:33:48 +0100

    Skeleton

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>filestack uploader</h1>
+    <FilesImporter/>
+  </div>
+</template>
+
+<script>
+import FilesImporter from "./components/FilesImporter.vue";
+export default {
+  name: "app",
+  components: {
+    FilesImporter
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
